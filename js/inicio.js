08d8f6f18d07685b34285608642e30b270d242a8
mrgarcia12 <104576878+mrgarcia12@users.noreply.github.com>
2022-05-27 22:44:05 -0600

    Delete js directory

--- js/inicio.js
+++
@@ -1,7 +0,0 @@
-var inicio = document.getElementById("inicio");
-var menu = document.getElementById("menu");
-
-function activar () {
-    menu.classList.add("menu-mostrar");
-}
-inicio.addEventListener("click", activar);
--- js/venta.js
+++
@@ -1,7 +0,0 @@
-var items = document.getElementById("items");
-var ventana = document.getElementById("ventana");
-
-function activar () {
-    ventana.classList.add("ventana-mostrar");
-}
-items.addEventListener("click", activar);
